88440c7602044f43b8c1d50f68fdb31b981f918a
danieltucunduva <danieltucunduva@users.noreply.github.com>
2018-05-31 08:25:33 -0400

    Back to Restify

--- src/api/api.js
+++ src/api/api.js
@@ -1,30 +1,18 @@
-const restify = require('restify');
-const corsMiddleware = require('restify-cors-middleware');
-const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
+const express = require('express');
+const server = express();
 
-const server = restify.createServer();
+server.get('*', (req, res) => {
+    res.send('Star');
+})
 
-const settings = require('./settings');
-const url = `mongodb://${settings.host}:${settings.port}/${settings.database}`;
-const connection = MongoClient.connect(url, { useNewUrlParser: true }).catch(error => console.error(error));
-
-const cors = corsMiddleware({
-    origins: ['http://localhost:4200', 'http://localhost:3000']
+server.get('/api/contacts', (req, res) => {
+    connection.then(response => {
+        const responseDB = response.db(settings.database);
+        const contactsCollection = responseDB.collection(settings.collection);
+        return contactsCollection.find({}).toArray();
+    }).then(response => res.json(response))
+        .catch(error => console.error(error));
 });
-
-server.pre(cors.preflight);
-server.use(cors.actual);
-server.use(restify.plugins.bodyParser());
-
-// server.get('/api/contacts', (req, res) => {
-//     connection.then(response => {
-//         const responseDB = response.db(settings.database);
-//         const contactsCollection = responseDB.collection(settings.collection);
-//         return contactsCollection.find({}).toArray();
-//     }).then(response => res.json(response))
-//         .catch(error => console.error(error));
-// });
 
 // server.get('/api/contacts/:id', (req, res) => {
 //     const objectID = req.params.id;
