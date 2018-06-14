f3820b722a44ca06e753c6738e483b3000807703
Proulx <maxime.proulx@cgi.com>
2018-06-14 10:12:04 -0400

    master -> graylog : merge (missing file)

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -1,165 +1,46 @@
 var userService = require("../services/user.service");
 
-//log management
-var graylog2 = require("graylog2");
-var logger = new graylog2.graylog({
-  servers: [{ host: "127.0.0.1", port: 12201 }]
-});
-exports.getUsers = async function(req, res, next) {
-  var page = req.query.page ? req.query.page : 1;
-  var limit = req.query.limit ? req.query.limit : 10;
-
-  logger.log(page, limit);
-
+/**
+ * @swagger
+ * /api/users/login:
+ *   post:
+ *     tags:
+ *      - users
+ *     parameters:
+ *       - name: user
+ *         description: User
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/User'
+ *     description: Log in a user
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: ok
+ *         schema:
+ *           type: object
+ *           properties:
+ *             status:
+ *               type: number
+ *             data:
+ *               $ref: '#/definitions/User'
+ *             message:
+ *               type: string
+ *       400:
+ *         description: bad request
+ *       406:
+ *         description: not acceptable
+ */
+exports.deleteData = async function(req, res, next) {
+  const userId = req.params.username;
   try {
-    var todos = await userService.getUsers({}, page, limit);
+    const sprintsDeleted = await userService.deleteData(userId);
     return res.status(200).json({
       status: 200,
-      data: todos,
-      message: "Succesfully Todos Recieved"
-    });
-  } catch (e) {
-    return res.status(400).json({
-      status: 400,
-      message: e.message
-    });
-  }
-};
-
-exports.createUser = async function(req, res, next) {
-  if (!req.body.username || !req.body.password) {
-    return res.status(400).json({
-      status: 400,
-      message: "Create user: error"
-    });
-  }
-
-  var newUser = {
-    username: req.body.username,
-    password: req.body.password
-  };
-
-  try {
-    var usernameAvailable = await userService.usernameAvailable(
-      newUser.username
-    );
-    if (!usernameAvailable) {
-      return res.status(409).json({
-        status: 409,
-        message: "Username already exists"
-      });
-    }
-    var createdUser = await userService.createUser(newUser);
-    return res.status(201).json({
-      status: 201,
-      data: createdUser,
-      message: "Create user: success"
-    });
-  } catch (e) {
-    return res.status(400).json({
-      status: 400,
-      message: "Create user: error"
-    });
-  }
-};
-
-exports.loginUser = async function(req, res, next) {
-  logger.log(req.body);
-
-  var user = {
-    username: req.body.username,
-    password: req.body.password
-  };
-
-  try {
-    var userFound = await userService.loginUser(user);
-    if (userFound) {
-      return res.status(200).json({
-        status: 200,
-        data: userFound,
-        message: "Login: success"
-      });
-    } else {
-      return res.status(406).json({
-        status: 406,
-        message: "Login: invalid"
-      });
-    }
-  } catch (e) {
-    return res.status(400).json({
-      status: 400,
-      message: e.message
-    });
-  }
-};
-
-exports.userId = async function(req, res, next) {
-  var user = {
-    username: req.body.username,
-    password: req.body.password
-  };
-  try {
-    var userId = await userService.userId(user);
-    if (userId) {
-      return res.status(200).json({
-        status: 200,
-        data: userId,
-        message: "User id: success"
-      });
-    } else {
-      return res.status(406).json({
-        status: 406,
-        message: "User: invalid"
-      });
-    }
-  } catch (e) {
-    return res.status(400).json({
-      status: 400,
-      message: e.message
-    });
-  }
-};
-
-exports.updateUser = async function(req, res, next) {
-  if (!req.body._id) {
-    return res.status(400).json({
-      status: 400,
-      message: "Id must be present"
-    });
-  }
-
-  var id = req.body._id;
-
-  var todo = {
-    id,
-    title: req.body.title ? req.body.title : null,
-    description: req.body.description ? req.body.description : null,
-    status: req.body.status ? req.body.status : null
-  };
-
-  try {
-    var updatedTodo = await userService.updateUser(todo);
-    return res.status(200).json({
-      status: 200,
-      data: updatedTodo,
-      message: "Success: sprint updated"
-    });
-  } catch (e) {
-    return res.status(400).json({
-      status: 400,
-      message: e.message
-    });
-  }
-};
-
-exports.deleteUser = async function(req, res, next) {
-  const username = req.params.username;
-  try {
-    await userService.deleteUser(username);
-    return res.status(200).json({
-      status: 200,
-      data: true,
-      message: "Success: user deleted"
+      data: sprintsDeleted,
+      message: "Success: data deleted"
     });
   } catch (e) {
     return res.status(400).json({
